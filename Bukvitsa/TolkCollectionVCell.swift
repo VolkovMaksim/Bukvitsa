7cfed6f150a376c1ca60e5eec4ab30ff71dbb760
PavlovMD <32665280+PavlovMD@users.noreply.github.com>
2020-04-02 08:21:10 +0300

    Add Tolk's VCs and files for Tolk's VCs

---
+++ Bukvitsa/TolkCollectionVCell.swift
@@ -0,0 +1,13 @@
+//
+//  TolkCollectionVCell.swift
+//  Bukvitsa
+//
+//  Created by Maksim Pavlov on 02.04.2020.
+//  Copyright © 2020 Maksim Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class TolkCollectionVCell: UICollectionViewCell {
+    
+}
---
+++ Bukvitsa/TolkVC.swift
@@ -0,0 +1,30 @@
+//
+//  TolkVC.swift
+//  Bukvitsa
+//
+//  Created by Maksim Pavlov on 02.04.2020.
+//  Copyright © 2020 Maksim Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class TolkVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
